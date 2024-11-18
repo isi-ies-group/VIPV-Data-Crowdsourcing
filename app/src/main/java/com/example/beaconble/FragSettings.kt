e9ae6d314a16c8d9009c87143208657c3de46d04
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-11-18 19:04:12 +0000

    little renamings

--- app/src/main/java/com/example/beaconble/FragSettings.kt
+++ app/src/main/java/com/example/beaconble/FragSettings.kt
@@ -1,14 +1,10 @@
 package com.example.beaconble
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.navigation.findNavController
 import androidx.preference.PreferenceFragmentCompat
 import android.widget.Toast
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class FragSettings : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
