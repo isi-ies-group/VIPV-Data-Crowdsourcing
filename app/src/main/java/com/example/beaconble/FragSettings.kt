6e110a9a8873aefe735207085fd19a29c641d19e
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-11-20 10:10:44 +0000

    prefs impros

--- app/src/main/java/com/example/beaconble/FragSettings.kt
+++ app/src/main/java/com/example/beaconble/FragSettings.kt
@@ -2,14 +2,11 @@ package com.example.beaconble
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
-import android.widget.Toast
 
 class FragSettings : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
-
-        Toast.makeText(context, "These are your settings", Toast.LENGTH_SHORT).show()
     }
 
 }
