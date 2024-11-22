10ff3cb8615f4d733c91dedd4eaa531680990d4f
echedey-ls <80125792+echedey-ls@users.noreply.github.com>
2024-11-22 08:17:12 +0000

    Fix DeprecationWarning

--- app/src/main/java/com/example/beaconble/FragAbout.kt
+++ app/src/main/java/com/example/beaconble/FragAbout.kt
@@ -18,12 +18,17 @@ class AboutFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
+    @Suppress("DEPRECATION")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         // Get the version name from the manifest
         val versionInfo = requireContext().packageManager.getPackageInfo(requireContext().packageName, 0)
         versionTextView = requireView().findViewById<TextView>(R.id.version_textview)
         // Set the version name and version code in the text view as: "Version: 1.0-1"
-        versionTextView.text = getString(R.string.version, versionInfo.versionName, versionInfo.versionCode)
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {  // versionInfo.versionCode was deprecated in API level 28. Use versionCodeLong instead.
+            versionTextView.text = getString(R.string.version, versionInfo.versionName, versionInfo.longVersionCode)
+        } else {
+            versionTextView.text = getString(R.string.version, versionInfo.versionName, versionInfo.versionCode)
+        }
     }
 }
